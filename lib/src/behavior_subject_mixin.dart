d4dc51c8129e50c0e5d7f178c60ab54c6529342e
heavenOSK <ryunosukeheaven.ba@gmail.com>
2020-08-08 12:59:11 +0900

    fix document comment

--- lib/src/behavior_subject_mixin.dart
+++ lib/src/behavior_subject_mixin.dart
@@ -1,8 +1,8 @@
 import 'package:rxdart/rxdart.dart';
 import 'package:state_notifier/state_notifier.dart';
 
-/// A mixin which provides [stream] interface of [BehaviorSubject]
-/// for [StateNotifier].
+/// A mixin which provides state of [StateNotifier] as a stream
+/// of [BehaviorSubject].
 mixin BehaviorSubjectMixin<T> on StateNotifier<T> {
   BehaviorSubject<T> _subject;
 
